4c2412244d00a8b1c8358c4fa94163f378ed6bc2
aihara <>
2022-04-11 12:51:35 +0900

    first commit

---
+++ src/pages/testing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Gallery from '../examples/Gallery'
+
+const Testing = () => {
+  return (
+    <Layout>
+      <Gallery />
+    </Layout>
+  )
+}
+
+export default Testing
